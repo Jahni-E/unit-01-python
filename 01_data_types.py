8052a586f62a6041a96f96624e79d4fbd63c8b7c
Jahni Eubanks <jahnie@brooklynsteamcenter.org>
2025-09-12 10:51:44 -0400

    formatting

---
+++ 01_data_types.py
@@ -0,0 +1,9 @@
+my_float: 2.56
+my_integer= int("my_float")
+print(my_float)
+print(my_integer)
+
+input1 = float("give me a float")
+num1 = float(input)
+print(type("num1"))
+
---
+++ 02_formating.py
@@ -0,0 +1,42 @@
+"""
+TASK 1:
+Write code that checks if a user entered the correct password.
+The password should not be case sensitive
+
+"""
+
+
+
+"""
+TASK 2:
+Write code that checks if a user inputs an empty string
+If the string is empty, print "invalid" otherwise print "valid"
+"""
+B = f
+
+"""
+TASK 3:
+
+Write a program that will replace the word "cat" with the word "dog"
+It should replace all occurances regardless of captilization 
+"""
+name = ("cat")
+new_name = name.replace("cat,dog")
+print(new_name)
+
+"""
+TASK 4:
+
+Write a program that takes a person's name and age as input and prints it
+"""
+name = "bob"
+age = "21"
+input = "bob" + str(age)
+print(input)
+
+"""
+TASK 5:
+
+Write a program that takes in two floats, and prints the quotient
+The result should be rounded to the nearest tenth (1 decimal place)
+"""
