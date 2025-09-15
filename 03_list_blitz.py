3f55ddc87688448fd25e09d8bcdb2a049eeaff22
Jahni Eubanks <jahnie@brooklynsteamcenter.org>
2025-09-15 10:51:26 -0400

    list

---
+++ 03_list_blitz.py
@@ -0,0 +1,34 @@
+# Task1
+numbers = [10,15,20,25]
+print(numbers)
+#Task2
+sports =["basketball","soccer","tennis"]
+sports.append("football")
+print(sports)
+#Task3
+sports =["basketball","soccer","tennis","football"]
+sports.remove("basketball")
+print(sports)
+#Task4
+sports = ["basketball","soccer",track]
+print(sports[1])
+sports[2] ="tennis"
+print(sports)
+#Task5
+games=[]
+games.append("fortnite")
+print(games)
+games.append("nba 2k26")
+print(games)
+#Task6
+sports =["basketball","soccer","tennis","football"]
+del sports[3]
+print(sports)
+#Task7
+sports =["basketball","soccer","tennis","football"]
+print(sports[0:2])
+#Task8
+sports =["basketball","soccer","tennis","football"]
+tvapps =["netflix","hbo","starzs"]
+entertainment = sports + tvapps
+print(entertainment)
