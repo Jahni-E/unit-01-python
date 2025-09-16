0a1677a6da10fa8c416179c50b79bfeb19b3bc05
Jahni Eubanks <jahnie@brooklynsteamcenter.org>
2025-09-16 10:52:06 -0400

    conditional

---
+++  04_condtional_calisthenics.py
@@ -0,0 +1,20 @@
+#Exercise1
+x = 14
+if x > 10:
+    print(True)
+else:
+    print(False)
+#Exercise2
+x = 15
+y = "student"
+if x < 18 or y:
+    print(10)
+else:
+    print(20)
+#Exercise3
+fruit = ["banna", "apple", "mango"]
+if "banna" in fruit:
+    print("yes")
+else:
+    print("no")
+#Exercise4
