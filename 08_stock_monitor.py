4146a965b1a9c1dc585716b79af575696aec9dac
Jahni Eubanks <jahnie@brooklynsteamcenter.org>
2025-09-26 10:03:44 -0400

    group work

---
+++ 08_stock_monitor.py
@@ -0,0 +1,22 @@
+stock_symbol = input("Enter stock symbol: ")
+current_price = float(input("Enter starting price: "))
+target_price = float(input("Enter target price: "))
+stop_loss = float(input("Enter stop-loss price: "))
+
+price_updates = 0
+alert_triggered = False
+
+while current_price > target_price:
+    current_price = float(input("Enter starting price: "))
+
+if current_price <= target_price:
+    print("Buy")
+    current_price = float(input("Enter starting price: "))
+
+if current_price >= stop_loss:
+    print("Sell")
+current_price = float(input("Enter starting price: "))
+
+if current_price == 0:
+    print("ERROR")
+
