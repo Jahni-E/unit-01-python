906d2447ac26eff418e369a2fac991e0a415bbcc
Jahni Eubanks <jahnie@brooklynsteamcenter.org>
2025-10-01 09:31:22 -0400

    project 1

--- todo.py
+++ todo.py
@@ -1,10 +1,12 @@
 t=[]
-
+L= 1
 while True:
     print("Your current todos are ")
     for x in t:
-        print(x)  
-    Q =input("Would you like to add or remove a todo or clear all")
+
+        print(f"{L}.{x}")
+        L += 1
+    Q =input("Would you like to add or remove a todo or clear all or edit")
     if Q == "add":
         n = input("What is your new todo")
         t.append(n)
