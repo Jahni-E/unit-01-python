1babab2af5aedcd718ff0a89143fc53a0670d106
Jahni Eubanks <jahnie@brooklynsteamcenter.org>
2025-10-01 10:51:57 -0400

    outstanding

--- todo.py
+++ todo.py
@@ -1,12 +1,16 @@
+with open("todos.txt") as file:
+ t = file.readlines()
+
 t=[]
 L= 1
+# Replace with open()
 while True:
     print("Your current todos are ")
     for x in t:
 
         print(f"{L}.{x}")
         L += 1
-    Q =input("Would you like to add or remove a todo or clear all or edit")
+    Q =input("Would you like to add or remove a todo or clear all")
     if Q == "add":
         n = input("What is your new todo")
         t.append(n)
@@ -15,3 +19,8 @@ while True:
         t.remove(x)
     if Q == "clear all":
         t=[]
+    if Q == "exit":
+        # Write to your file
+        with open("todos.txt") as file:
+            file.writelines(t)
+        break
